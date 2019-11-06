06f7555a71f61d6cafaf0ef02a4ac2833bac8e35
Unknown <nicholasmaude@gmail.com>
2019-11-06 17:33:41 -0500

    write data to Real-Time database

--- Proxy/app/src/main/java/com/example/myapplication/ARCdata.java
+++ Proxy/app/src/main/java/com/example/myapplication/ARCdata.java
@@ -5,31 +5,37 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class ARCdata extends AppCompatActivity {
+public class ARCdata extends AppCompatActivity implements View.OnClickListener {
     private ProgressBar mprogressbar;
     private int mprogressbarStat =0;
-
+    public int roomnum =0;
     private Handler handler = new Handler();
     private TextView full;
     private TextView notbusy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arcdata);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setOnClickListener();
      /*
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -73,5 +79,64 @@ public class ARCdata extends AppCompatActivity {
         Intent intent =new Intent(this,second.class);
         startActivity(intent);
     }
+
+    public void readData(){
+
+
+    }
+
+
+    public void database(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("room number " + roomnum);
+        roomnum++;
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("room number " + roomnum, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("room number " + roomnum, "Failed to read value.", error.toException());
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        database();
+
+        switch (view.getId()) {
+            case R.id.button6:
+         //       database();
+        }
+    }
+    private void setOnClickListener(){
+        findViewById(R.id.r110).setOnClickListener(this);
+        findViewById(R.id.r112).setOnClickListener(this);
+        findViewById(R.id.r131).setOnClickListener(this);
+        findViewById(R.id.r147).setOnClickListener(this);
+        findViewById(R.id.r149).setOnClickListener(this);
+        findViewById(R.id.r151).setOnClickListener(this);
+        findViewById(R.id.r153).setOnClickListener(this);
+        findViewById(R.id.r214).setOnClickListener(this);
+        findViewById(R.id.r216).setOnClickListener(this);
+        findViewById(R.id.r217).setOnClickListener(this);
+        findViewById(R.id.r219).setOnClickListener(this);
+
+
+
+    }
+
 
 }
