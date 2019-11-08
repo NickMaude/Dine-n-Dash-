f7cc8ed8a046671785818e555b77ce95be6fa726
Unknown <nicholasmaude@gmail.com>
2019-11-08 15:49:25 -0500

    write occupy study room data to firebase

--- Proxy/app/src/main/java/com/example/myapplication/ARCdata.java
+++ Proxy/app/src/main/java/com/example/myapplication/ARCdata.java
@@ -36,17 +36,7 @@ public class ARCdata extends AppCompatActivity implements View.OnClickListener {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setOnClickListener();
-     /*
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
-  */
         mprogressbar = (ProgressBar) findViewById(R.id.progressBar);
 
         new Thread(new Runnable() {
@@ -86,11 +76,12 @@ public class ARCdata extends AppCompatActivity implements View.OnClickListener {
     }
 
 
-    public void database(int roomnum){
+    public void database(int roomnum, int isoccupied){
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.setValue("room number " + roomnum);
-        //roomnum++;
+        DatabaseReference myRef = database.getReference(Integer.toString(roomnum));
+        myRef.setValue(Integer.toString(isoccupied));
+        //myRef.child("room number" + roomnum);
+
 
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
@@ -118,104 +109,137 @@ public class ARCdata extends AppCompatActivity implements View.OnClickListener {
 
         switch (view.getId()) {
             case R.id.r110:
-                database(110);
                 Button r110 =  (Button)findViewById(R.id.r110);
                 if(r110.getText()=="Occupy"){
                     r110.setText("Occupied");
+                    database(110,1);
+
                 } else{
                     r110.setText("Occupy");
+                    database(110,0);
+
                 }
                 break;
             case R.id.r112:
-                database(112);
                 Button r112 =  (Button)findViewById(R.id.r112);
                 if(r112.getText()=="Occupy"){
                     r112.setText("Occupied");
+                    database(112,1);
+
                 } else{
                     r112.setText("Occupy");
+                    database(112,0);
+
                 }
                 break;
             case R.id.r131:
-                database(131);
+                database(131,1);
                 Button r131 =  (Button)findViewById(R.id.r131);
                 if(r131.getText()=="Occupy"){
                     r131.setText("Occupied");
+                    database(131,1);
+
                 } else{
                     r131.setText("Occupy");
+                    database(131,0);
+
                 }
                 break;
 
             case R.id.r147:
-                database(141);
+                database(141,1);
                 Button r147 =  (Button)findViewById(R.id.r147);
                 if(r147.getText()=="Occupy"){
                     r147.setText("Occupied");
+                    database(141,1);
+
                 } else{
                     r147.setText("Occupy");
+                    database(141,0);
+
                 }
                 break;
 
 
             case R.id.r149:
-                database(149);
                 Button r149 =  (Button)findViewById(R.id.r149);
                 if(r149.getText()=="Occupy"){
                     r149.setText("Occupied");
+                    database(149,1);
+
                 } else{
                     r149.setText("Occupy");
+                    database(149,0);
+
                 }
                 break;
 
             case R.id.r151:
-                database(151);
                 Button r151 =  (Button)findViewById(R.id.r151);
                 if(r151.getText()=="Occupy"){
                     r151.setText("Occupied");
+                    database(151,1);
+
                 } else{
                     r151.setText("Occupy");
+                    database(151,0);
+
                 }
                 break;
 
 
             case R.id.r214:
-                database(214);
                 Button r214 =  (Button)findViewById(R.id.r214);
                 if(r214.getText()=="Occupy"){
                     r214.setText("Occupied");
+                    database(214,1);
+
                 } else{
                     r214.setText("Occupy");
+                    database(214,0);
+
                 }
                 break;
 
             case R.id.r216:
-                database(216);
                 Button r216 =  (Button)findViewById(R.id.r216);
                 if(r216.getText()=="Occupy"){
                     r216.setText("Occupied");
+                    database(216,1);
+
                 } else{
                     r216.setText("Occupy");
+                    database(216,0);
+
                 }
                 break;
 
 
             case R.id.r217:
-                database(217);
                 Button r217 =  (Button)findViewById(R.id.r217);
                 if(r217.getText()=="Occupy"){
                     r217.setText("Occupied");
+                    database(217,1);
+
                 } else{
                     r217.setText("Occupy");
+                    database(217,0);
+
                 }
                 break;
 
 
             case R.id.r219:
-                //database(219);
                 Button r219 =  (Button)findViewById(R.id.r219);
                 if(r219.getText()=="Occupy"){
                     r219.setText("Occupied");
+                    database(219,1);
+
+
                 } else{
                     r219.setText("Occupy");
+                    database(219,0);
+
                 }
                 break;
 
